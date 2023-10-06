fd9ba6e534a5e2c92d57cfdbedd2774d0414c310
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-10-06 22:43:45 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        
+        public string EmployeeProfile(int id)
+
+
+        {
+            string Profile = string.Empty;
+            if(id==1)
+            {
+                Profile = "Employe profile 1";
+            }
+            else if(id==2)
+            {
+                Profile = "Employe profile 2";
+            }
+            else
+            {
+                Profile = "Employe profile empty";
+            }
+
+            return Profile;
+        }
+
+        public string Address(int id,string Department)
+        {
+            return "id=" + id + " "+"department=" + Department;
+
+        }
+    }
+}
